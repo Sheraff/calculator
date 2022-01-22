86ac98d800e3112439b7da07af3692ae1c8f7b89
Sheraff <Sheraff@users.noreply.github.com>
2022-01-22 15:27:44 +0000

    deploy: 2095698089dfa7275a2b7f166e8c75258ccb0b8a

--- src/assets/scripts/MathParser/classes/Token.js
+++
@@ -1,9 +0,0 @@
-export default class Token {
-	constructor({
-		value = '', 
-		inputRange = [],
-	}) {
-		this.value = value
-		this.inputRange = inputRange
-	}
-}
--- src/assets/scripts/MathParser/utils/factorial.js
+++
@@ -1,5 +0,0 @@
-export default function factorial(n) {
-	return n < 2
-		? 1 
-		: factorial(n - 1) * n
-}
