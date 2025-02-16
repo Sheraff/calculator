584ee3673d6e9bea29d0c3d2ba9fbbae043ef6bb
Sheraff <fpellet@ensc.fr>
2025-02-16 17:03:04 +0100

    Implement Kahan summation for OrBinaryOperatorNode to improve precision in calculations

--- src/assets/scripts/MathParser/plugins/OrBinaryOps.js
+++ src/assets/scripts/MathParser/plugins/OrBinaryOps.js
@@ -1,4 +1,5 @@
-import {add, subtract} from '../utils/floatMath'
+/* eslint-disable no-undef -- bigint is undefined because TS isn't configured on this repo */
+
 import BinaryOperatorPlugin from './BinaryOperators'
 
 class OrBinaryOperatorToken extends BinaryOperatorPlugin.token {}
@@ -9,14 +10,55 @@ export default class OrBinaryOperatorPlugin extends BinaryOperatorPlugin {
 	static node = OrBinaryOperatorNode
 	static operators = ['+', '-']
 
+	/**
+	 * Resolve the entire tree of nodes down to every node that is also an OrBinaryOperatorNode.
+	 * This allows for Kahan summation to be performed on the entire tree.
+	 * @see https://en.wikipedia.org/wiki/Kahan_summation_algorithm
+	 * 
+	 * for example, `1 + 10^100 - 10^100` will return `1` instead of `0`
+	 *
+	 * @param {OrBinaryOperatorNode} node
+	 */
 	static resolve(node) {
-		const left = node.nodes[0].computed
-		const right = node.nodes[1].computed
-		switch (node.value) {
-			case '+':
-				return add(left, right)
-			case '-':
-				return subtract(left, right)
+		// Collect all terms for Kahan summation
+		/** @type {bigint[]} */
+		const terms = []
+		const signs = []
+
+		/** @param {import('../classes/Node').default} node */
+		function collectTerms(node, sign = 1) {
+			if (node instanceof OrBinaryOperatorNode) {
+				const left = node.nodes?.[0]
+				const right = node.nodes?.[1]
+				if (!left || !right) throw new Error('Cannot resolve node')
+				if (node.value === '+') {
+					collectTerms(left, sign)
+					collectTerms(right, sign)
+				} else if (node.value === '-') {
+					collectTerms(left, sign)
+					collectTerms(right, -sign)
+				}
+			} else {
+				terms.push(BigInt(node.computed))
+				signs.push(sign)
+			}
 		}
+
+		// Start collection with the current operation
+		collectTerms(node)
+
+		// Perform Kahan summation
+		let sum = BigInt(0)
+		let compensation = BigInt(0) // Running compensation for lost low-order bits
+
+		for (let i = 0; i < terms.length; i++) {
+			const term = terms[i] * BigInt(signs[i])
+			const y = (term - compensation)
+			const t = (sum + y)
+			compensation = t - sum - y
+			sum = t
+		}
+
+		return Number(sum)
 	}
 }
